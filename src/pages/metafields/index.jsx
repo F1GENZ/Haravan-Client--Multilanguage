b8d7ce835b33634ce227c7b3025e3c4376a71581
Devs2-Stores <individual.devs2@gmail.com>
2026-03-11 18:01:05 +0700

    feat: add back navigation button with product/collection title on translator page

--- src/pages/metafields/index.jsx
+++ src/pages/metafields/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useSearchParams } from "react-router";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { Button, Tabs, Flex, Spin, Space, message, Modal, Checkbox, Popover } from 'antd';
-import { TranslationOutlined, ExclamationCircleOutlined, CopyOutlined, SettingOutlined } from '@ant-design/icons';
+import { TranslationOutlined, ExclamationCircleOutlined, CopyOutlined, SettingOutlined, ArrowLeftOutlined } from '@ant-design/icons';
 import { useQueryClient } from '@tanstack/react-query';
 import { metafieldsService } from '../../common/MetafieldsServices';
 import { dataService } from '../../common/DataServices';
@@ -375,7 +375,16 @@ const Metafields = () => {
     <div className='space-y-2!'>
       <div className='sticky top-0 h-min-content bg-white p-4 shadow-md z-10'>
         <Flex justify='space-between' align='center' gap={16}>
-          <h2 className='text-lg text-red-600 font-semibold mb-0!'>⁉ Chuyển đổi ngôn ngữ</h2>
+          <Space>
+            <Link to="/products">
+              <Button icon={<ArrowLeftOutlined />} type="default">
+                {type === 'product' ? '← Sản phẩm' : '← Bộ sưu tập'}
+              </Button>
+            </Link>
+            <h2 className='text-lg text-red-600 font-semibold mb-0!'>
+              {item_data?.title || 'Chuyển đổi ngôn ngữ'}
+            </h2>
+          </Space>
           <Space>
             <Button 
               icon={<CopyOutlined />}
